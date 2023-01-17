faede93ef4d6f12323d7f890092d66200140c66e
Henrique <frizerahenrique@gmail.com>
2023-01-16 22:41:15 -0300

    estrutura com while para aprendizado

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play(); 
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]; 
+    const idAudio = `#som_${instrumento}`; //template string 
+    //como ele vai pegar a classe dos botões e transformar de forma composta na id dos sons? Ele pega só o texto.
+
+    //console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    };
+
+    contador ++;
+
+    //console.log (contador);
 }
+
+
+
