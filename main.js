10d5ae2d1543767cb7d8aaac7f4894d7a24bb768
Henrique <frizerahenrique@gmail.com>
2023-01-17 17:14:41 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom (idElementAudio) {
-    document.querySelector(idElementAudio).play(); 
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio); 
+
+    if (elemento /*!= null (não precisa pois o if no JavaScript já é inteligente para fazer essa verificação, se elemento existe ja vai cair como uma condição verdadeira*/ && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');        
+    }
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,4 +23,19 @@ for (i=0; i< listaDeTeclas.length; i++) {
         tocaSom(idAudio);
     };
 
+    tecla.onkeydown = function(evento) {
+        
+        //console.log(evento.code === 'Space');
+
+        if (evento.code === 'Space' || evento.code === 'Enter')  {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+    
 }
+    
